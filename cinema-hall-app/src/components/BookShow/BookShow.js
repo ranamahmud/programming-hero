c9f852bbda1112ce1936c0891fc186895f037f21
Md Rana Mahmud <ranamd0@gmail.com>
2020-12-20 13:09:17 +0600

    buttons added

--- cinema-hall-app/src/components/BookShow/BookShow.js
+++ cinema-hall-app/src/components/BookShow/BookShow.js
@@ -2,10 +2,21 @@ import React from 'react';
 import Header from '../Home/Header/Header';
 
 const BookShow = () => {
+    const columns = [1, 2, 3, 4, 5];
+    const rows = [1, 2, 3, 4, 5, 6, 7, 8];
     return (
         <div>
             <Header />
             <h1>BookShow</h1>
+            <div>
+                {
+                    columns.map(col =>
+                        <div>{
+                            rows.map(row => <button>{col}" "{row}</button>)
+                        }</div>
+                    )
+                }
+            </div>
         </div>
     );
 };
