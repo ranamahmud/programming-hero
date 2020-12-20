4e6b7fe9c8947e717b61853c4d2ec0e77b77449c
Md Rana Mahmud <ranamd0@gmail.com>
2020-12-20 12:29:39 +0600

    date box added

--- cinema-hall-app/src/components/Movie/Movie.js
+++ cinema-hall-app/src/components/Movie/Movie.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Image } from 'react-bootstrap';
-
+import DayPicker from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
 const Movie = ({ movie }) => {
+    console.log(movie.startDate);
+    console.log(movie.endDate);
     return (
         <div className="col-md-6" id="event" id={movie._id}>
             <div className="row">
@@ -10,6 +13,17 @@ const Movie = ({ movie }) => {
                     <h1>{movie.name}</h1>
                     <p>{movie.activityDate}</p>
                     {/* <button onClick={(e) => cancelEvent(e, movie._id)}>Cancel</button> */}
+                    <DayPicker
+                        canChangeMonth={false}
+                        initialMonth={new Date(movie.startDate)}
+                        disabledDays={[
+
+                            {
+                                after: new Date(movie.endDate),
+                                before: new Date(movie.startDate),
+                            },
+                        ]}
+                    />
 
                 </div>
             </div>
