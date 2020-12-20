a3e437675cc49cfcad9a91df41ef7ecfdbf73483
Md Rana Mahmud <ranamd0@gmail.com>
2020-12-20 14:21:34 +0600

    select seat button added

--- cinema-hall-app/src/components/BookShow/BookShow.js
+++ cinema-hall-app/src/components/BookShow/BookShow.js
@@ -1,22 +1,36 @@
 import React from 'react';
+import { Button, Col, Container, Image, Row } from 'react-bootstrap';
 import Header from '../Home/Header/Header';
 
-const BookShow = () => {
-    const columns = [1, 2, 3, 4, 5];
+const BookShow = (props) => {
+    const columns = ["A", "B", "C", "D", "E"];
     const rows = [1, 2, 3, 4, 5, 6, 7, 8];
     return (
         <div>
             <Header />
-            <h1>BookShow</h1>
-            <div>
-                {
-                    columns.map(col =>
-                        <div>{
-                            rows.map(row => <button>{col}" "{row}</button>)
-                        }</div>
-                    )
-                }
-            </div>
+            <Container>
+                <h1>BookShow</h1>
+                <div>
+                    {
+                        columns.map(col =>
+                            <Row>{
+                                rows.map(row =>
+
+                                    <Col>
+                                        <Button
+                                            style={{
+                                                margin: "5px"
+                                            }}
+                                        >
+                                            <p>Seat: {col}{row}</p>
+                                            <Image src={require("../../images/ticket-alt-solid.png").default} fluid /></Button>
+                                    </Col>
+                                )
+                            }</Row>
+                        )
+                    }
+                </div>
+            </Container>
         </div>
     );
 };
--- cinema-hall-app/src/components/Movie/Movie.js
+++ cinema-hall-app/src/components/Movie/Movie.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { Image } from 'react-bootstrap';
+import { Button, Image } from 'react-bootstrap';
 import DayPicker from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
+import { Link } from 'react-router-dom';
 const Movie = ({ movie }) => {
     const [selectedDay, setselectedDay] = useState();
 
@@ -13,9 +14,13 @@ const Movie = ({ movie }) => {
     return (
         <div className="col-md-6" id="event" id={movie._id}>
             <div className="row">
-                <div className="col-md-4"> <Image src={require(`../../posters/${movie.pic}`).default} fluid /></div>
+                <div className="col-md-4"> <Image src={require(`../../posters/${movie.pic}`).default} fluid />
+                    <p style={{
+                        fontSize: "25px"
+                    }}>{movie.name}</p>
+                </div>
                 <div className="col-md-4">
-                    <h1>{movie.name}</h1>
+
                     <p>{movie.activityDate}</p>
                     {/* <button onClick={(e) => cancelEvent(e, movie._id)}>Cancel</button> */}
                     <h5 className="text-center">Select Show Date</h5>
@@ -32,7 +37,37 @@ const Movie = ({ movie }) => {
                         onDayClick={handleDayClick}
                         selectedDays={selectedDay}
                     />
-
+                    <p>Show Time:</p>
+                    <select className="custom-select" id="inputGroupSelect01"
+                    // defaultValue={service.status}
+                    // style={{
+                    //     color: colors[service.status]
+                    // }}
+                    // onChange={handleChange}
+                    // id={service._id}
+                    // key={service._id}
+                    >
+                        <option value="9am"
+                        // style={{ color: colors['Done'] }}
+                        >09:00AM</option>
+                        <option value="12pm"
+                        // style={{ color: colors['On Going'] }}
+                        >12:00PM</option>
+                        <option value="3pm"
+                        // style={{ color: colors['Pending'] }}
+                        >03:00PM</option>
+                        <option value="6pm">
+                            06:00PM
+                        </option>
+                    </select>
+                    <Link to={"/book-show/" + movie._id}>
+                        <Button
+                            style={{
+                                marginTop: "10px",
+                                marginLeft: "10px"
+                            }}
+                        >Select Seat</Button>
+                    </Link>
                 </div>
             </div>
         </div >
